de0a82af58ec478f014d2487e529953f5fd2314b
Matheus Guida <159961710+Nevek20@users.noreply.github.com>
2025-03-12 11:55:28 -0300

    Add files via upload

---
+++ Assets/php/header.php
@@ -0,0 +1,15 @@
+<?php
+echo '
+    <body>
+        <header>
+            <nav class="opcoes">
+                <img src="Assets/Img/Logo.png" alt="Logo GameXchange">
+                <ul>
+                    <li><a href="index.html">Store</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Suporte</a></li>
+                </ul>
+                <button class="login-btn" onclick="window.location.href=\'login1.html\'">Entrar</button>
+            </nav>
+        </header> ';
+?>
