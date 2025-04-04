635d52819a998afa8280dc20fd64d15399856547
Ryan <ryanlimagermano@gmail.com>
2025-04-04 09:38:03 -0300

    Add files via upload

--- Assets/php/header.php
+++ Assets/php/header.php
@@ -2,10 +2,17 @@
         <img src="Assets/Img/Logo.png" alt="Logo GameXchange">
         <nav class="opcoes1">
             <ul>
-                <li><a href="./store.php">Store</a></li>
+                <li><a href="./index.php">Store</a></li>
                 <li><a href="./sobre.php">Sobre</a></li>
                 <li><a href="./suporte.php">Suporte</a></li>
             </ul>
-            <button class="login-btn" onclick="window.location.href='login1.php'">Entrar</button>
+            <div class="user-menu">
+                <?php if ($usuario_logado): ?>
+                    <span class="user-text">Olá, <?php echo htmlspecialchars($usuario_logado); ?></span>
+                    <a href="logout.php" class="btn-login logout-btn">Sair</a>
+                <?php else: ?>
+                    <a href="login1.php" class="btn-login">Entrar</a>
+                <?php endif; ?>
+            </div>
         </nav>
-</header>
+    </header>
